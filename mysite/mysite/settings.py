75b383b4b8e06dfdbebc128aaf1999169a765137
zhangxin <xinzhang125@163.com>
2020-03-28 23:03:58 +0800

    创建新应用

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'article',
     'image',
     'sorl.thumbnail',
+    'course',
+    
 
 ]
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('article/',include('article.urls',namespace='article')),
     path('home/', TemplateView.as_view(template_name='home.html'),name='home'),
     path('image/', include('image.urls',namespace='image')),
+    path('course/',include('course.urls',namespace='course')),
 
 ]
 
